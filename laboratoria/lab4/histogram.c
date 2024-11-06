bc4d7f798cc12642e5d5f3e907dfd71da0b5a48c
chox7 <chorzelakacper@gmail.com>
2024-11-06 13:33:15 +0100

    rozwiazano zadanie histogram

--- laboratoria/lab4/histogram.c
+++ laboratoria/lab4/histogram.c
@@ -2,30 +2,74 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define NSUM 11
+#define SCALE 20
+
 int randof(int n) {
     return (int) (rand() / (RAND_MAX + 1.0) * n);
 }
 
-int main() { 
+int znajdz_maks(int A[], int n) {
+    int max = 0;
+    for (int i = 0; i < n; i++) {
+        if (A[i] > max) {
+            max = A[i];
+        }
+    }
+    return max;
+}
+
+void ramka_pozioma() {
+    printf("+");
+    for (int i = 0; i < NSUM + 2; i++) {
+       printf("-");
+    }
+    printf("+");
+    printf("\n");
+}
+
+int main() {
     srand((unsigned) time(NULL));
 
     int n;
     scanf("%d", &n);
 
-    int m = 11; // możliwe sumy: 2,3,4,5,6,7,8,9,10,11,12
-    int A[m];
-    for (int i = 0; i < m; i++) {
+    // Inicializacja listy sumowan
+    int A[NSUM];
+    for (int i = 0; i < NSUM; i++) {
         A[i] = 0;
     }
 
-    for (int i = 0; i < n; i++) { 
+    // Losowanie n razy
+    for (int i = 0; i < n; i++) {
         int sum = randof(6) + 1 + randof(6) + 1;
         A[sum - 2]++;
     }
-    
-    for (int i = 0; i < m; i++) {
-        printf("%d\n", A[i]);
+
+    // Szukanie maksimum histogramu
+    int max = znajdz_maks(A, NSUM);
+
+    // Ramka gora
+    ramka_pozioma();
+
+    // Histogram
+    for (int i = SCALE; i > 0; i--) {
+        // Ramka lewo
+        printf("| ");
+        for (int j = 0; j < NSUM; j++) {
+            if (A[j] * SCALE / max >= i) {
+                printf("*");
+            } else {
+                printf(" ");
+            }
+        }
+        // Ramka prawo
+        printf(" |");
+        printf("\n");
     }
+
+    // Ramka dol
+    ramka_pozioma();
 
     return 0;
 }
